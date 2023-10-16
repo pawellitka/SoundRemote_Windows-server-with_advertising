4a897bdeaf554d923eb9f4075742150017cdd5e6
ashipo <24320267+ashipo@users.noreply.github.com>
2023-10-16 11:30:45 +0300

    Fix includes in EncoderOpus
    Moved #include "AudioUtil.h" from .cpp to .h

--- SoundRemote/EncoderOpus.cpp
+++ SoundRemote/EncoderOpus.cpp
@@ -4,7 +4,6 @@
 #include <opus/opus.h>
 
 #include "Util.h"
-#include "AudioUtil.h"
 #include "EncoderOpus.h"
 
 namespace {
--- SoundRemote/EncoderOpus.h
+++ SoundRemote/EncoderOpus.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "AudioUtil.h"
+
 struct OpusEncoder;
 
 struct EncoderDeleter {
