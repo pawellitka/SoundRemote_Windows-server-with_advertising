cf55eb34aa446a481565fa34abe3b80ca1a82039
ashipo <24320267+ashipo@users.noreply.github.com>
2023-10-21 15:08:14 +0300

    Add icons for the mute button

--- SoundRemote/resource.h
+++ SoundRemote/resource.h
@@ -16,6 +16,8 @@
 #define IDS_KEYSTROKES                  108
 #define IDC_SOUNDREMOTE                 109
 #define IDR_MAINFRAME                   128
+#define IDI_SOUND_ON                    131
+#define IDI_SOUND_OFF                   132
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -23,7 +25,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
