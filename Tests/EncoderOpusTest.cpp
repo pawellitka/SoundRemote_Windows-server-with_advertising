7142dbf75d46d60bc2b7500b4bcd6f801effd4f9
ashipo <24320267+ashipo@users.noreply.github.com>
2023-10-25 20:38:29 +0300

    Update EncoderOpusTest

--- Tests/EncoderOpusTest.cpp
+++ Tests/EncoderOpusTest.cpp
@@ -9,20 +9,20 @@ namespace {
 	public:
 		void SetUp() override { sampleRate_ = GetParam(); }
 	protected:
-		SampleRate sampleRate_ = SampleRate::_8k;
+		SampleRate sampleRate_ = {};
 	};
 
 	TEST_P(EncoderOpusTest, CreatesWithValidSampleRate)  {
-		EXPECT_NO_THROW(EncoderOpus(sampleRate_, Channels::mono));
-		EXPECT_NO_THROW(EncoderOpus(sampleRate_, Channels::stereo));
+		EXPECT_NO_THROW(EncoderOpus(Audio::Bitrate::kbps_128, sampleRate_, Channels::mono));
+		EXPECT_NO_THROW(EncoderOpus(Audio::Bitrate::kbps_128, sampleRate_, Channels::stereo));
 	}
 
 	INSTANTIATE_TEST_SUITE_P(SampleRates, EncoderOpusTest, ::testing::Values(
-		SampleRate::_8k,
-		SampleRate::_12k,
-		SampleRate::_16k,
-		SampleRate::_24k,
-		SampleRate::_48k
+		SampleRate::khz_8,
+		SampleRate::khz_12,
+		SampleRate::khz_16,
+		SampleRate::khz_24,
+		SampleRate::khz_48
 	), [](const testing::TestParamInfo<EncoderOpusTest::ParamType>& info) {
 		const int value = static_cast<int>(info.param);
 		return std::to_string(value);
